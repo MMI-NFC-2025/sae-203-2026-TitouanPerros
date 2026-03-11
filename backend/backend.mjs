e72ef5e41252d2416ff8501a824230027e51b4a3
TitouanPerros <titouan.perros02@edu.univ-fcomte.fr>
2026-03-11 19:23:57 +0100

    correction faute header

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -3,93 +3,61 @@ const pb = new PocketBase('https://flyingtapfest.titouan-perros.fr:443');
 
 //Fonction 1 : retourne la liste de tous les artistes triés par date de représentation
 export async function getAllArtists() {
-    try{
         const artists = await pb.collection('artiste').getFullList({
             sort: 'date',
         });
         return artists;
-    } catch (error) {
-        console.error('Erreur lors de la récupération des artistes :', error);
-    }
 }
 
 //Fonction 2 : retourne la liste de toutes les scènes triées par nom
 export async function getAllScenes() {
-    try{
         const scenes = await pb.collection('scene').getFullList({
             sort: 'nom',
         });
         return scenes;
-    } catch (error) {
-        console.error('Erreur lors de la récupération des scènes :', error);
-    }
 }
 
 //Fonction 3 : retourne la liste de tous les artistes triés par ordre alphabétique
 export async function getAllArtistsSorted() {
-    try{
         const artists = await pb.collection('artiste').getFullList({
             sort: 'nom',
         });
         return artists;
-    } catch (error) {
-        console.error('Erreur lors de la récupération des artistes :', error);
-    }
 }
 
 //Fonction 4 : retourne les infos d'un artiste à partir de son ID
 export async function getArtistByID(id) {
-    try{
+  
         const artist = await pb.collection('artiste').getOne(id);
         return artist;
-    } catch (error) {
-        console.error('Erreur lors de la récupération de l\'artiste :', error);
-    }
-}
-
+} 
 //Fonction 5 : retourne les infos d'une scène à partir de son ID
 export async function getSceneByID(id) {
-    try{
         const scene = await pb.collection('scene').getOne(id);
         return scene;
-    } catch (error) {
-        console.error('Erreur lors de la récupération de la scène :', error);
-    }  
 }
 
 //Fonction 6 : retourne tous les artistes se produisant sur une scène donnée par son id, triés par date
 export async function getArtistsBySceneID(sceneID) {
-    try{
         const artists = await pb.collection('artiste').getFullList({
             filter: `field = "${sceneID}"`,
             sort: 'date',
         });
         return artists;
-    } catch (error) {
-        console.error('Erreur lors de la récupération des artistes :', error);
-    }
 }
 //Fonction 7 : retourne tous les artistes se produisant sur une scène donnée par son nom, triés par date
 export async function getArtistsBySceneName(sceneName) {
-    try{
         const artists = await pb.collection('artiste').getFullList({
             filter: `scene = "${sceneName}"`,
             sort: 'date',
         });
         return artists;
-    } catch (error) {
-        console.error('Erreur lors de la récupération des artistes :', error);
-    }
 }
 
 //Fonction 8 : permet d'ajouter ou modifier les informations d'un artiste ou d'une scène
 export async function upsertArtist(artist) {
-    try{
         const record = await pb.collection('artiste').create(artist);
         return record;
-    } catch (error) {
-        console.error('Erreur lors de l\'ajout ou de la modification de l\'artiste :', error);
-    }
 }
 
 //Fonction pour le PBimage
